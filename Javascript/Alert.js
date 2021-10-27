043de8d8f6e3401d5034c4b243b0b2c7d4aaca82
mike7sa <76029358+mike7sa@users.noreply.github.com>
2021-10-27 16:20:03 -0300

    Version 1 TPO

---
+++ Javascript/Alert.js
@@ -0,0 +1,11 @@
+Swal.fire({
+    title:"Bienvenido" ,
+   // icon:'question' ,
+   // icon:'warning' ,
+    //icon:'error' ,
+   // icon:'info' ,
+    icon:'success' ,
+    footer: 'Gracias por ayudar a salvar el planeta'
+    
+
+});
